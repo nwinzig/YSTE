d4ef46148a7280375a1aa6070d0654fef1449cfc
Noah <noahwinzig@gmail.com>
2022-11-12 15:10:47 -0500

    get one product route functioning

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -3,13 +3,25 @@ from app.models import Product, db
 
 products_routes = Blueprint('product', __name__)
 
-
+#get all products
 @products_routes.route('/')
 def index():
+    """get all products, to be used on splash page """
     products = Product.query.all()
-    print('before dict',products)
+
     newProducts = []
     newProducts.extend([i.to_dict() for i in products])
 
-    print('after dict', newProducts)
+
     return {'Products':newProducts}
+
+
+#get one product
+@products_routes.route('/<int:id>')
+def singularProduct(id):
+    """get one product, to be used for product detail page """
+
+    product = Product.query.get(id)
+
+    newProduct = product.to_dict()
+    return {'Product': newProduct}
