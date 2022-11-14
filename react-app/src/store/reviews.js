34c081eccf05ef48474258dd909716f35ed06960
calebflores <calebflores1388@gmail.com>
2022-11-14 13:34:34 -0700

    review in p

--- react-app/src/store/reviews.js
+++ react-app/src/store/reviews.js
@@ -1,9 +1,41 @@
 // Actions
+const LOAD_REVIEWS = 'reviews/LOAD_REVIEWS'
 
 //Action Creators
+const loadAll = (reviews) => {
+    return {
+        'type': LOAD_REVIEWS,
+        reviews
+    }
+}
 
 // Thunks
+export const getAllReviews = (id) => async dispatch => {
+    const response = await fetch(`api/products/${id}/reviews`)
+
+    if (response.ok) {
+        const reviews = await response.json()
+        console.log('prodcut reviews', reviews)
+        dispatch(loadAll(reviews))
+        return reviews
+    }
+}
 
 // Initial State
+let initialState = {}
 
 // Reducer
+const reviewsReducer = (state = initialState, action) => {
+    let newState = {}
+    switch (action.type) {
+        case LOAD_REVIEWS: {
+            newState = { ...action.reviews }
+            return newState
+        }
+        default:
+            return state
+    }
+}
+
+
+export default reviewsReducer
