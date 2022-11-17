6533ee53ca03bd1cb9c8c0c3ce461e54ed3f9c99
Trevor <Trevor1798@yahoo.com>
2022-11-17 17:36:24 -0500

    adding two more imports to env.py

--- migrations/env.py
+++ migrations/env.py
@@ -3,6 +3,9 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from flask import current_app
 
 from alembic import context
