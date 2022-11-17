c055c02c57703c13c9932ab2af70838e8c4ca212
Trevor <Trevor1798@yahoo.com>
2022-11-17 17:05:46 -0500

    changing migrations

--- migrations/versions/37eedb86b8d8_.py
+++ migrations/versions/37eedb86b8d8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 07bea76bf538
+Revision ID: 37eedb86b8d8
 Revises: 
-Create Date: 2022-11-17 01:20:23.530211
+Create Date: 2022-11-17 17:04:10.542714
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '07bea76bf538'
+revision = '37eedb86b8d8'
 down_revision = None
 branch_labels = None
 depends_on = None
