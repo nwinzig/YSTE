0177e50841f084feb9c621c7c11dd442d6c111da
Noah <noahwinzig@gmail.com>
2022-11-17 12:03:26 -0500

    updating the team

--- react-app/src/components/ProductForm/index.js
+++ react-app/src/components/ProductForm/index.js
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom'
 let categories = [{ value: 'Cars' }, { value: 'Clothing' }, { value: 'Electronics' }, { value: 'Home Goods' }, { value: 'miscellaneous' }]
 function ProductForm() {
     const prodlist = useSelector(state => state.products.products)
-    console.log('###########', prodlist)
+    // console.log('###########', prodlist)
     const [productName, setProductName] = useState('')
     const [description, setDescription] = useState('')
     const [price, setPrice] = useState('')
@@ -21,11 +21,12 @@ function ProductForm() {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-
+        let priceInt = parseFloat(price)
+        let newprice = (Math.round(priceInt * 100) / 100).toFixed(2)
         let newProduct = {
             'product_name': productName,
             description,
-            price,
+            'price': newprice,
             category,
             stock: 1,
             image1,
@@ -49,15 +50,15 @@ function ProductForm() {
         <form onSubmit={handleSubmit}>
             <div>
                 <label>Product Name</label>
-                <input type='text' placeholder="Product Name" value={productName} onChange={(e) => setProductName(e.target.value)} />
+                <input required type='text' placeholder="Product Name" value={productName} onChange={(e) => setProductName(e.target.value)} />
             </div>
             <div>
                 <label>Description</label>
-                <input type='text' placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <input required type='text' placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
             </div>
             <div>
                 <label>Price</label>
-                <input type='text' placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
+                <input required type='number' min={1} step='0.01' placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
             </div>
             <div>
                 <label>Image Url</label>
